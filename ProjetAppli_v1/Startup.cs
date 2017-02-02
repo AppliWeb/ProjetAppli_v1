8943e114348f59dc900d727667d2439bf5406990
UTILISA-TKF4NPR\Administrateur <lololovemelo@hotmail.com>
2017-02-02 12:03:03 +0100

    Ajoutez des fichiers projet.

---
+++ ProjetAppli_v1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjetAppli_v1.Startup))]
+namespace ProjetAppli_v1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
